9c4f95ea62004b2438fcc1cc4553fdd6f1ba6935
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-07-15 21:55:26 -0300

    chore: mongodb connection

---
+++ src/connection/mongodb.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const connectMongoDB = async () => {
+    const { DB_USER, DB_PASSWORD, DB_CLUSTER, DB_NAME } = process.env
+    const urlMongoDB = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}/${DB_NAME}?retryWrites=true&w=majority`
+
+    await mongoose.connect(
+        urlMongoDB,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            useFindAndModify: false
+        }
+    )
+}
+
+module.exports = connectMongoDB;
