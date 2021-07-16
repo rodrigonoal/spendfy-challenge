4d08adfc41e7fbc85a5d2864ac315bf8c5232679
RODRIGO NOAL <rodrigonoal@gmail.com>
2021-07-15 21:57:41 -0300

    chore: middleware

---
+++ src/middleware/base64converter.js
@@ -0,0 +1,21 @@
+const base64 = require('base-64');
+const utf8 = require('utf8');
+
+
+function base64converter(req, res, next) {
+    const { content } = req.body;
+
+    const bytes = utf8.encode(content);
+
+    const encoded = base64.encode(bytes);
+
+    const fileSize = (encoded.length * 6) / 8000;
+
+    req.body.content = encoded;
+
+    req.body.kbSize = fileSize;
+
+    return next();
+}
+
+module.exports = base64converter;
